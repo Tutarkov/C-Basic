e6cac808dd3fe4615ee411019dad7a30696f9708
Tutarkov <ktutarkov@outlook.com>
2023-03-12 18:42:05 +0100

    Homework03 and Homework04

---
+++ Homework03/Homework03/Homework03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine();
+
+        if (input.Length <5)
+        {
+            Console.WriteLine("Input is not valid");
+        }
+        else
+        {
+            string lastFive = input.Substring(input.Length - 5);
+            Console.WriteLine("Last 5 characters are {0}", lastFive);
+        }
+    }
+}
